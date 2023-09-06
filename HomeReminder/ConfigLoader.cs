a4252688350379ce3267c730136a0a83516affc7
vinh trantx <vinh.trantx@hotmail.com>
2023-09-06 17:08:21 +1000

    Add project files.

---
+++ HomeReminder/ConfigLoader.cs
@@ -0,0 +1,48 @@
+﻿namespace Reminder
+{
+    public struct MessageByTime
+    {
+        public string messageText;
+        public string Time;
+    }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class Config
+    {
+        public DateOnly[] SpecificDates { set; get; }
+        public DayOfWeek[] DayOfWeeks { set; get; }
+        public MessageByTime[] Messages { set; get; }
+
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class ConfigLoader
+    {
+        public static IList<Config> GetConfigs() {
+
+            List<Config> result = new() {
+                new Config()
+                {
+                    SpecificDates = Array.Empty<DateOnly>(),
+                    DayOfWeeks = new DayOfWeek[]
+                    {
+                        DayOfWeek.Monday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday
+                    },
+                    Messages = new MessageByTime[]
+                    {
+                        new MessageByTime
+                        {
+                            messageText = "Cook Dinner",
+                            Time = "17:15"
+                        },
+                        new MessageByTime
+                        {
+                            messageText = "Internet is going down soon",
+                            Time = "23:40"
+                        }
+                    }
+                }
+            };
+            
+            return result;
+        }
+    }
+}
---
+++ HomeReminder/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Reminder;
+
+var audio = new ToastAudio
+{
+    Loop = true
+};
+ConfigLoader configLoader = new ConfigLoader();
+IList<Config> schedules = ConfigLoader.GetConfigs();
+foreach (var schedule in schedules)
+{
+    foreach (var date in schedule.SpecificDates)
+    {
+        if (date == DateOnly.FromDateTime(DateTime.Today))
+        {
+            ScheduleMessage(schedule.Messages);
+            break;
+        }
+    }
+    foreach(var dow in schedule.DayOfWeeks)
+    {
+        if (dow == DateTime.Today.DayOfWeek)
+        {
+            ScheduleMessage(schedule.Messages);
+            break;
+        }
+    }
+}
+
+
+void ScheduleMessage(MessageByTime[] messages)
+{
+    foreach (var message in messages)
+    {
+        var dismissSnoozeActions = new ToastActionsSnoozeAndDismiss();
+        var msg1 = new ToastContentBuilder()
+            .AddText(message.messageText)
+            .AddAudio(audio)
+            .SetToastScenario(ToastScenario.Alarm);
+        msg1.Content.Actions = dismissSnoozeActions;
+        msg1.Schedule(DateTimeOffset.Parse(message.Time));
+    }
+}
